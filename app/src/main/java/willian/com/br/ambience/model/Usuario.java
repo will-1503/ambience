da6aa51062850b8e5319a9d0d636c19bc67ca658
will-1503 <willianantoniodasilva06@gmail.com>
2025-03-24 11:43:05 -0300

    Rev01

---
+++ app/src/main/java/willian/com/br/ambience/model/Usuario.java
@@ -0,0 +1,20 @@
+package willian.com.br.ambience.model;
+
+public class Usuario {
+    //atributos
+    private String usuario;
+    private String nome;
+    private String idade;
+    private String data;
+
+
+
+    //métodos
+    public String  getUsuario(){
+        return this.usuario;
+    }
+
+    publicvoid setgetUsuario(String gt){
+        if gt.length
+    }
+}
