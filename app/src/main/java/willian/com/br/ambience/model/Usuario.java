0399c5b8bf0008e6a281e4e5eedd08b5d1d22fd7
will-1503 <willianantoniodasilva06@gmail.com>
2025-03-31 11:39:44 -0300

    Click do botão Salvar

--- app/src/main/java/willian/com/br/ambience/model/Usuario.java
+++ app/src/main/java/willian/com/br/ambience/model/Usuario.java
@@ -1,20 +1,88 @@
 package willian.com.br.ambience.model;
 
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Objects;
+
 public class Usuario {
     //atributos
-    private String usuario;
+    private String email;
     private String nome;
-    private String idade;
-    private String data;
+    private int idade;
+    private String senha;
 
 
-
-    //métodos
-    public String  getUsuario(){
-        return this.usuario;
+    //CONSTRUTOR - Inicializa os atributos para gerar Objeto Json
+    public Usuario () throws Exception {
+        this.setNome("");
+        this.setEmail("");
+        this.setIdade(0);
+        this.setSenha("");
+    }
+    //CONSTRUTOR - inicializa atributos de um arquivo JSon
+    public Usuario (JSONObject jp) {
+        try {
+            this.setEmail(jp.getString("email"));
+            this.setNome(jp.getString("nome"));
+            this.setIdade(jp.getInt("idade"));
+            this.setSenha(jp.getString("senha"));
+        } catch (Exception e) {
+            Log.e("Usuario", Objects.requireNonNull(e.getMessage()));
+        }
     }
 
-    publicvoid setgetUsuario(String gt){
-        if gt.length
+    //Metodo retorna o objeto com dados no formato JSON
+    public JSONObject toJsonObject() {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("email", this.email);
+            json.put("nome", this.nome);
+            json.put("idade", this.idade);
+            json.put("senha", this.senha);
+
+        } catch (JSONException e) {
+            Log.e("Usuario", Objects.requireNonNull(e.getMessage()));
+        }
+        return json;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 }
+
+
--- app/src/main/java/willian/com/br/ambience/ui/usuario/CadUsuarioFragment.java
+++ app/src/main/java/willian/com/br/ambience/ui/usuario/CadUsuarioFragment.java
@@ -1,5 +1,6 @@
 package willian.com.br.ambience.ui.usuario;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import willian.com.br.ambience.R;
+import willian.com.br.ambience.model.Usuario;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CadUsuarioFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CadUsuarioFragment extends Fragment {
+public class CadUsuarioFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +30,13 @@ public class CadUsuarioFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private View view;
+    private EditText etNome;
+    private EditText etEmail;
+    private EditText etIdade;
+    private EditText etSenha;
+    private Button btSalvar;
+
 
     public CadUsuarioFragment() {
         // Required empty public constructor
@@ -61,6 +73,38 @@ public class CadUsuarioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cad_usuario, container, false);
+        this.view = inflater.inflate(R.layout.fragment_cad_usuario, container, false);
+      //Binding
+        this.etNome = view.findViewById(R.id.etNome);
+        this.etEmail = view.findViewById(R.id.etEmail);
+        this.etIdade = view.findViewById(R.id.etIdade);
+        this.etSenha = view.findViewById((R.id.etSenha));
+        this.btSalvar = view.findViewById(R.id.btSalvar);
+        //definindo o listener do botão
+        this.btSalvar.setOnClickListener(this);
+      //RETORNO DA FUNÇÃO
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btSalvar){
+            //instancear meu objeto de negócio
+            try {
+                Usuario usuario = new Usuario();
+                //pegar dados da tela e por no objeto
+                usuario.setNome(this.etNome.getText().toString());
+                usuario.setEmail(this.etEmail.getText().toString());
+                int idade = Integer.parseInt(this.etIdade.getText().toString());
+                usuario.setIdade(idade);
+                usuario.setSenha(this.etSenha.getText().toString());
+                //mensagem de sucesso
+                Toast.makeText(view.getContext(), "Sucesso", Toast.LENGTH_LONG ).show();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 }
+
