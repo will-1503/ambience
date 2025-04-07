9d4ceb168c548cb207fc9d23181c2d2b77ddeabb
will-1503 <willianantoniodasilva06@gmail.com>
2025-04-07 10:50:39 -0300

    cadastro de usuario finalizado

--- app/src/main/java/willian/com/br/ambience/ui/usuario/CadUsuarioFragment.java
+++ app/src/main/java/willian/com/br/ambience/ui/usuario/CadUsuarioFragment.java
@@ -12,6 +12,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import willian.com.br.ambience.R;
 import willian.com.br.ambience.model.Usuario;
 
@@ -20,7 +31,7 @@ import willian.com.br.ambience.model.Usuario;
  * Use the {@link CadUsuarioFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CadUsuarioFragment extends Fragment implements View.OnClickListener {
+public class CadUsuarioFragment extends Fragment implements View.OnClickListener, Response.ErrorListener, Response.Listener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,6 +47,9 @@ public class CadUsuarioFragment extends Fragment implements View.OnClickListener
     private EditText etIdade;
     private EditText etSenha;
     private Button btSalvar;
+    //volley
+    private RequestQueue requestQueue;
+    private JsonObjectRequest jsonObjectReq;
 
 
     public CadUsuarioFragment() {
@@ -74,7 +88,7 @@ public class CadUsuarioFragment extends Fragment implements View.OnClickListener
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         this.view = inflater.inflate(R.layout.fragment_cad_usuario, container, false);
-      //Binding
+        //Binding
         this.etNome = view.findViewById(R.id.etNome);
         this.etEmail = view.findViewById(R.id.etEmail);
         this.etIdade = view.findViewById(R.id.etIdade);
@@ -82,13 +96,17 @@ public class CadUsuarioFragment extends Fragment implements View.OnClickListener
         this.btSalvar = view.findViewById(R.id.btSalvar);
         //definindo o listener do botão
         this.btSalvar.setOnClickListener(this);
-      //RETORNO DA FUNÇÃO
+        //instanciando a fila de requests - caso o objeto seja o view
+        this.requestQueue = Volley.newRequestQueue(view.getContext());
+        //inicializando a fila de requests do SO
+        this.requestQueue.start();
+        //RETORNO DA FUNÇÃO
         return view;
     }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.btSalvar){
+        if (view.getId() == R.id.btSalvar) {
             //instancear meu objeto de negócio
             try {
                 Usuario usuario = new Usuario();
@@ -98,13 +116,59 @@ public class CadUsuarioFragment extends Fragment implements View.OnClickListener
                 int idade = Integer.parseInt(this.etIdade.getText().toString());
                 usuario.setIdade(idade);
                 usuario.setSenha(this.etSenha.getText().toString());
+                //REQUEST VOLLEY AQUI !!!!!!!
+                jsonObjectReq = new JsonObjectRequest(
+                        Request.Method.POST,
+                        "http://10.0.2.2:8080/seg/cadusuario.php",
+                        usuario.toJsonObject(), this, this);
+                requestQueue.add(jsonObjectReq);
+
                 //mensagem de sucesso
-                Toast.makeText(view.getContext(), "Sucesso", Toast.LENGTH_LONG ).show();
+                Toast.makeText(view.getContext(), "Sucesso", Toast.LENGTH_LONG).show();
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
 
         }
     }
+
+
+    @Override
+    public void onResponse(Object response) {
+        try {
+//instanciando objeto para manejar o JSON que recebemos
+            JSONObject jason = new JSONObject(response.toString());
+            //context e text são para a mensagem na tela o Toast
+            Context context = view.getContext();
+//pegando mensagem que veio do json
+            CharSequence mensagem = jason.getString("message");
+//duração da mensagem na tela
+            int duration = Toast.LENGTH_SHORT;
+            //verificando se salvou sem erro para limpar campos da tela
+            if (jason.getBoolean("success")) {
+//limpar campos da tela
+                this.etNome.setText("");
+                this.etSenha.setText("");
+                this.etIdade.setText("");
+                this.etEmail.setText("");
+            }
+//mostrando a mensagem que veio do JSON
+            Toast toast = Toast.makeText(context, mensagem, duration);
+            toast.show();
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Snackbar mensagem = Snackbar.make(view,
+                "Ops! Houve um problema ao realizar o cadastro: " +
+                        error.toString(), Snackbar.LENGTH_LONG);
+        mensagem.show();
+    }
 }
+
 
